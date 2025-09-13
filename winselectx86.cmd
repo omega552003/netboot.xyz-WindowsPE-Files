b88bbbe88ee9e5b4f3d8bd471ced41b344647909
omega552003 <omega552003@users.noreply.github.com>
2025-09-13 13:52:27 -0400

    Update winselectx86.cmd
    added Windows XP setup.

--- winselectx86.cmd
+++ winselectx86.cmd
@@ -7,6 +7,8 @@ echo 2. Windows 8.1 x86
 echo 3. Windows 8 x86
 echo 4. Windows 7 SP1 x86
 echo 5. Windows Vista SP2 x86
+echo 6. Windows XP Home Edition SP2 x86
+echo 7. Windows XP Professional SP2 x86
 echo.
 echo 0. Quit
 echo r  Restart System
@@ -19,14 +21,24 @@ if %winver% == 1 ( set WIN=Windows_10-x86
 ) else ( if %winver% == 3 ( set WIN=Windows_80-x86
 ) else ( if %winver% == 4 ( set WIN=Windows_7-x86
 ) else ( if %winver% == 5 ( set WIN=Windows_Vista-x86
+) else ( if %winver% == 6 (
+    set WIN=Windows_XpH-x86
+    goto winxp
+) else ( if %winver% == 7 (
+    set WIN=Windows_XpP-x86
+    goto winxp
 ) else ( if %winver% == 0 ( exit /b
 ) else ( if %winver% == r ( exit
 ) else (
     echo invalid choice
     goto select
-)))))))
+)))))))))
 
 echo Starting Windows Setup...
+Y:\NetBoot\%WIN%\setup.exe
+goto select
 
-Y:\%WINPATH%\%WIN%\setup.exe
-
+:winxp
+echo Starting Windows Setup...
+Y:\NetBoot\%WIN%\i386\winnt32.exe
+goto select
